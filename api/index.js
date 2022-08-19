cd7e14ca6053e80f2272a3b2a535bb6283f1bb6f
EarliestFall <howelltaylor195@gmail.com>
2022-08-18 23:03:00 -0500

    removed mongoose, just in case

--- api/index.js
+++ api/index.js
@@ -4,9 +4,9 @@ const app = express()
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-mongoose.connect(process.env.DBCONNECTION, { useNewUrlParser: true }, () => {
-    console.log("connected to db!")
-})
+// mongoose.connect(process.env.DBCONNECTION, { useNewUrlParser: true }, () => {
+//     console.log("connected to db!")
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
