bf718a75bb1355a85e849110509f39ead49e5549
EarliestFall <howelltaylor195@gmail.com>
2022-08-20 10:40:34 -0500

    fixing CORS policy'

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,8 @@ module.exports = {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://saftisolutionsapp.azurewebsites.net/',
+    browserBaseURL: 'http://localhost:3000'
   },
 
 /*
