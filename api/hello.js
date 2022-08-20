9a22a8d563e2d9f63c18159eb07f156e3e2fed2f
Taylor Howell <howelltaylor195@gmail.com>
2022-08-20 16:03:58 -0500

    trying to understand the limits of functions

---
+++ api/hello.js
@@ -0,0 +1,29 @@
+
+// const express = require('express')
+// const app = express()
+// const mongoose = require('mongoose');
+// require('dotenv/config');
+
+
+
+// // mongoose.connect(process.env.DBCONNECTION, { useNewUrlParser: true }, () => {
+// //     console.log("connected to db!")
+// // })
+
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+
+// app.get('/', function (req, res) {
+//   res.send('Test successful')
+// })
+
+// export default {
+//   path: '/api',
+//   handler: app
+// }
+
+
+export default function handler(request, response) {
+  const { name } = request.query;
+  response.end(`Hello ${name}!`);
+}
