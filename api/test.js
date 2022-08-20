34f736803720c992387b230db1894fe1dfee44cf
Taylor Howell <howelltaylor195@gmail.com>
2022-08-20 15:47:00 -0500

    changing stuff to push API limits

---
+++ api/test.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const cors = requre('cors')
+require('dotenv/config');
+
+
+
+// mongoose.connect(process.env.DBCONNECTION, { useNewUrlParser: true }, () => {
+//     console.log("connected to db!")
+// })
+
+app.use(cors)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+  res.send('Test successful')
+})
+
+export default {
+  path: '/api',
+  handler: app
+}
+
+
