d1413742826b379f1b2e704cdc365b35b85afb54
Taylor Howell <howelltaylor195@gmail.com>
2022-08-20 15:54:42 -0500

    fixed issue with json

--- api/test.js
+++ api/test.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
-const cors = requre('cors')
+const cors = require('cors')
 require('dotenv/config');
 
 
